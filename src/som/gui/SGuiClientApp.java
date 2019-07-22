355f58a85b505a93c17937f6c9cb3939e03dc302
Sergio Flores <floresgtz@hotmail.com>
2019-07-22 10:56:54 -0500

    2019-07-22, 1.0 063.0 Sergio Abraham FLORES GUTIÉRREZ 1) Implementación de báscula predeterminada, y mejoramiento a mail de notificación de recepción de materias primas (boleto tarado), para optimizar la generación de información excluyendo secciones de báscula si no son necesarias, i.e., si la recepción correspondiente a cada sección fue hecha en más de una báscula o en una báscula no predeterminada.

--- src/som/gui/SGuiClientApp.java
+++ src/som/gui/SGuiClientApp.java
@@ -67,7 +67,7 @@ import som.mod.cfg.db.SDbUserGui;
 public class SGuiClientApp extends JFrame implements SGuiClient, ActionListener {
 
     public static final String APP_NAME = "SOM 1.0";
-    public static final String APP_RELEASE = "SOM 1.0 062.2"; // release date: 2019-07-11
+    public static final String APP_RELEASE = "SOM 1.0 063.0"; // release date: 2019-07-22
     public static final String APP_COPYRIGHT = "2013-2019";
     public static final String APP_PROVIDER = "Software Aplicado SA de CV";
 
