56d47431196ff0ecf306fd9a39d8b471dffab5a7
Edwin Carmona <eedwincarmona@gmail.com>
2019-10-02 17:00:14 -0500

    Actualización de log y versión

--- src/som/gui/SGuiClientApp.java
+++ src/som/gui/SGuiClientApp.java
@@ -67,7 +67,7 @@ import som.mod.cfg.db.SDbUserGui;
 public class SGuiClientApp extends JFrame implements SGuiClient, ActionListener {
 
     public static final String APP_NAME = "SOM 1.0";
-    public static final String APP_RELEASE = "SOM 1.0 063.4"; // release date: 2019-08-16
+    public static final String APP_RELEASE = "SOM 1.0 063.5"; // release date: 2019-10-02
     public static final String APP_COPYRIGHT = "2013-2019";
     public static final String APP_PROVIDER = "Software Aplicado SA de CV";
 
