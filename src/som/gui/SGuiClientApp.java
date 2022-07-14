0712a4a438fb56299b492e529566d3ba044aa6b8
Sergio Flores <sflores@swaplicado.com.mx>
2022-07-14 12:46:05 -0500

    2022-07-14, 1.0 077.9/ Sergio Abraham FLORES GUTIÉRREZ/ 1) Adición de suscripción a Suprarregiones en vista de Regiones.

--- src/som/gui/SGuiClientApp.java
+++ src/som/gui/SGuiClientApp.java
@@ -67,7 +67,7 @@ import som.mod.cfg.db.SDbUserGui;
 public class SGuiClientApp extends JFrame implements SGuiClient, ActionListener {
 
     public static final String APP_NAME = "SOM 1.0";
-    public static final String APP_RELEASE = "SOM 1.0 077.8"; // release date: 2022-06-24
+    public static final String APP_RELEASE = "SOM 1.0 077.9"; // release date: 2022-07-14
     public static final String APP_COPYRIGHT = "2013-2022";
     public static final String APP_PROVIDER = "Software Aplicado SA de CV";
 
