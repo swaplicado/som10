328090b13126ad8199b479c739edcd2644852fb3
Isabel García <isabel.garcia@swaplicado.com.mx>
2022-12-01 14:14:21 -0600

    Update SGuiClientApp.java

--- src/som/gui/SGuiClientApp.java
+++ src/som/gui/SGuiClientApp.java
@@ -68,7 +68,7 @@ import som.mod.cfg.db.SDbUserGui;
 public class SGuiClientApp extends JFrame implements SGuiClient, ActionListener {
 
     public static final String APP_NAME = "SOM 1.0";
-    public static final String APP_RELEASE = "SOM 1.0 082.0"; // release date: 2022-11-04
+    public static final String APP_RELEASE = "SOM 1.0 083.0"; // release date: 2022-12-01
     public static final String APP_COPYRIGHT = "2013-2022";
     public static final String APP_PROVIDER = "Software Aplicado SA de CV";
 
