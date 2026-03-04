9fa6be6f1f547ff739dfe5e1eaedafd120d5e0a2
Sergio Flores <sflores@swaplicado.com.mx>
2026-03-04 12:24:00 -0600

    Release 1.0 097.3.

--- src/som/gui/SGuiClientApp.java
+++ src/som/gui/SGuiClientApp.java
@@ -68,7 +68,7 @@ import som.mod.cfg.db.SDbUserGui;
 public class SGuiClientApp extends JFrame implements SGuiClient, ActionListener {
 
     public static final String APP_NAME = "SOM 1.0";
-    public static final String APP_RELEASE_NUM = "097.2"; // release date: 2026-03-03
+    public static final String APP_RELEASE_NUM = "097.3"; // release date: 2026-03-04
     public static final String APP_RELEASE = APP_NAME + " " + APP_RELEASE_NUM;
     public static final String APP_COPYRIGHT = "2013-2026";
     public static final String APP_PROVIDER = "Software Aplicado SA de CV";
